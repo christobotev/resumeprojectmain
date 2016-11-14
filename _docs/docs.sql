7374c7fed61a052470a2ededc37ff6c2fc1e062e
H.Botev <h.botev@viscomp.bg>
2016-11-14 03:13:16 +0200

    update the db dump - there are missing permission (because of missing db migrations)

--- _docs/docs.sql
+++ _docs/docs.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 22, 2016 at 01:33 AM
+-- Generation Time: Nov 14, 2016 at 03:12 AM
 -- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.17
+-- PHP Version: 5.5.9-1ubuntu4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,15 +39,16 @@ CREATE TABLE IF NOT EXISTS `Appointments` (
   KEY `User` (`userID`),
   KEY `withUser` (`withUserID`),
   KEY `statusID` (`statusID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `Appointments`
 --
 
 INSERT INTO `Appointments` (`appointmentID`, `statusID`, `userID`, `withUserID`, `scheduled`, `created`, `noteID`) VALUES
-(1, 1, 3, 3, '2016-08-23 07:05:00', '2016-08-21 22:16:30', 1),
-(2, 1, 3, 4, '2016-08-23 08:00:00', '2016-08-21 22:17:14', 2);
+(1, 3, 3, 3, '2016-08-23 07:05:00', '2016-08-21 22:16:30', 1),
+(2, 1, 3, 4, '2016-08-23 08:00:00', '2016-08-21 22:17:14', 2),
+(3, 1, 19, 5, '2016-10-15 08:00:00', '2016-10-14 17:42:35', 4);
 
 -- --------------------------------------------------------
 
@@ -83,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `AppointmentSymptoms` (
   PRIMARY KEY (`appointmentSymptomsID`),
   KEY `symptomID` (`symptomID`),
   KEY `appointmentID` (`appointmentID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `AppointmentSymptoms`
@@ -95,7 +96,9 @@ INSERT INTO `AppointmentSymptoms` (`appointmentSymptomsID`, `appointmentID`, `sy
 (3, 1, 6),
 (4, 2, 2),
 (5, 2, 4),
-(6, 2, 9);
+(6, 2, 9),
+(7, 3, 4),
+(8, 3, 6);
 
 -- --------------------------------------------------------
 
@@ -133,7 +136,9 @@ INSERT INTO `migration_versions` (`version`) VALUES
 ('20160501215743'),
 ('20160501220316'),
 ('20160505102010'),
-('20160507123358');
+('20160507123358'),
+('20161014204012'),
+('20161015213759');
 
 -- --------------------------------------------------------
 
@@ -148,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `Notes` (
   `userID` int(11) NOT NULL,
   PRIMARY KEY (`noteID`),
   KEY `userID` (`userID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `Notes`
@@ -157,7 +162,18 @@ CREATE TABLE IF NOT EXISTS `Notes` (
 INSERT INTO `Notes` (`noteID`, `content`, `created`, `userID`) VALUES
 (1, 'test 123', '2016-08-21 22:16:30', 3),
 (2, 'test 234', '2016-08-21 22:17:14', 3),
-(3, 'reminder to myself', '2016-08-21 22:20:01', 3);
+(3, 'reminder to myself', '2016-08-21 22:20:01', 3),
+(4, 'ghhg', '2016-10-14 17:42:35', 19),
+(5, 'hyhyhy', '2016-10-15 19:08:14', 19),
+(6, 'yuiti', '2016-10-15 22:39:04', 3),
+(7, 'yuiti', '2016-10-15 22:39:26', 3),
+(8, 'yuiti', '2016-10-15 22:39:32', 3),
+(9, 'test 14141', '2016-10-15 22:41:41', 3),
+(10, 'test 14141', '2016-10-15 22:41:48', 3),
+(11, 'jrtjrtjrtjtr', '2016-10-15 22:42:58', 3),
+(12, 'test 121fsa', '2016-10-15 22:43:58', 3),
+(13, 'wgewgweg', '2016-10-15 23:01:03', 3),
+(14, '1234', '2016-11-13 23:59:27', 3);
 
 -- --------------------------------------------------------
 
@@ -196,14 +212,24 @@ CREATE TABLE IF NOT EXISTS `Reminders` (
   KEY `userID` (`userID`),
   KEY `noteID` (`noteID`),
   KEY `createdByUserID` (`createdBy`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `Reminders`
 --
 
 INSERT INTO `Reminders` (`reminderID`, `userID`, `noteID`, `scheduled`, `status`, `created`, `createdBy`) VALUES
-(1, 4, 3, '2016-08-23 00:00:00', 0, '2016-08-21 22:20:01', 3);
+(1, 4, 3, '2016-08-23 00:00:00', 0, '2016-08-21 22:20:01', 3),
+(2, 3, 5, '2016-10-16 00:00:00', 0, '2016-10-15 19:08:14', 19),
+(3, 3, 6, '2016-10-19 00:00:00', 0, '2016-10-15 22:39:04', 3),
+(4, 3, 7, '2016-10-19 00:00:00', 0, '2016-10-15 22:39:26', 3),
+(5, 3, 8, '2016-10-19 00:00:00', 0, '2016-10-15 22:39:32', 3),
+(6, 3, 9, '2016-10-18 00:00:00', 0, '2016-10-15 22:41:41', 3),
+(7, 3, 10, '2016-10-18 00:00:00', 0, '2016-10-15 22:41:48', 3),
+(8, 3, 11, '2016-10-17 00:00:00', 0, '2016-10-15 22:42:58', 3),
+(9, 3, 12, '2016-10-20 00:00:00', 0, '2016-10-15 22:43:58', 3),
+(10, 3, 13, '2016-10-18 00:00:00', 0, '2016-10-15 23:01:03', 3),
+(11, 3, 14, '2016-11-15 00:00:00', 0, '2016-11-13 23:59:27', 3);
 
 -- --------------------------------------------------------
 
@@ -215,7 +241,7 @@ CREATE TABLE IF NOT EXISTS `Resources` (
   `resourceID` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`resourceID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `Resources`
@@ -237,7 +263,9 @@ INSERT INTO `Resources` (`resourceID`, `name`) VALUES
 (15, 'Docs_MainBundle_Controller_AppointmentsExportController_exportAction'),
 (16, 'Docs_MainBundle_Controller_AppointmentUpdateController_approveAction'),
 (17, 'Docs_MainBundle_Controller_AppointmentUpdateController_closeAction'),
-(18, 'Docs_MainBundle_Controller_RatingController_listAction');
+(18, 'Docs_MainBundle_Controller_RatingController_listAction'),
+(20, 'Docs_MainBundle_Controller_AppointmentUpdateController_declineAction'),
+(22, 'Docs_MainBundle_Controller_RemindersController_addReminderModalAction');
 
 -- --------------------------------------------------------
 
@@ -253,7 +281,7 @@ CREATE TABLE IF NOT EXISTS `RoleResources` (
   PRIMARY KEY (`roleResourceID`),
   KEY `IDX_DAFA81A9B79B5C79` (`resourceID`),
   KEY `IDX_DAFA81A983ACDD40` (`roleID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=51 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=57 ;
 
 --
 -- Dumping data for table `RoleResources`
@@ -309,7 +337,13 @@ INSERT INTO `RoleResources` (`roleResourceID`, `rights`, `resourceID`, `roleID`)
 (47, NULL, 18, 1),
 (48, NULL, 18, 2),
 (49, NULL, 18, 3),
-(50, NULL, 18, 5);
+(50, NULL, 18, 5),
+(51, NULL, 20, 1),
+(52, NULL, 20, 2),
+(53, NULL, 20, 3),
+(54, NULL, 20, 5),
+(55, NULL, 22, 5),
+(56, NULL, 14, 2);
 
 -- --------------------------------------------------------
 
@@ -411,7 +445,7 @@ CREATE TABLE IF NOT EXISTS `UserRoles` (
   PRIMARY KEY (`userRoleID`),
   KEY `roleID` (`roleID`),
   KEY `userID` (`userID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=25 ;
 
 --
 -- Dumping data for table `UserRoles`
@@ -433,7 +467,13 @@ INSERT INTO `UserRoles` (`userRoleID`, `userID`, `roleID`) VALUES
 (15, 12, 5),
 (16, 13, 5),
 (17, 14, 5),
-(18, 15, 5);
+(18, 15, 5),
+(19, 17, 2),
+(20, 18, 2),
+(21, 19, 2),
+(22, 20, 2),
+(23, 21, 2),
+(24, 22, 2);
 
 -- --------------------------------------------------------
 
@@ -456,7 +496,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
   UNIQUE KEY `UNIQ_D5428AEDF85E0677` (`username`),
   UNIQUE KEY `UNIQ_D5428AEDE7927C74` (`email`),
   UNIQUE KEY `UNIQ_D5428AED8FFBE0F7` (`salt`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `Users`
@@ -465,11 +505,11 @@ CREATE TABLE IF NOT EXISTS `Users` (
 INSERT INTO `Users` (`userID`, `firstName`, `lastName`, `username`, `password`, `email`, `salt`, `is_active`, `googleID`, `created`) VALUES
 (1, 'hristo', 'botev', 'hbotev', '$2y$10$f29552efe949a425738bdu3FGONKljnQzMQr1rPbRqLPjxGekxuAK', 'hbotev@gmail.com', 'bS3KpLAQar', 1, '0', '2016-05-02 21:00:00'),
 (2, 'hristo', 'botev', 'hbotev2', '$2y$10$5alR2amqY/K0LxcgxQ/5veba3Uxc8edo4M6xN7v0fo9MDm4e2AheC', 'hbotev2@abv.bg', 'a6245234f4a7313fa330c90eb7414043', 1, '0', '2016-05-02 21:00:00'),
-(3, 'Hristo', 'Botev', 'Hristo Botev', NULL, 'icoo0oobotev@gmail.com', NULL, 1, '110532046243401050166', '0000-00-00 00:00:00'),
-(4, 'Dummy', 'last 1', 'dummyDoc1', '$2y$10$wKQ5Xe5yHT2OmqqEqYHui.oINYAnMaoO2BCSkdBIEjpCdbFhlSkLS', 'dummy@email.com', 'e271390a9e497368fb3e1aea775b8292', 1, '0', '0000-00-00 00:00:00'),
+(3, 'Hristo', 'Botev', 'Hristo Botev', NULL, 'icoo0oobotev@gmail.com', NULL, 1, '110532046243401050166', '2016-05-02 21:00:00'),
+(4, 'Dummy', 'last 1', 'dummyDoc1', '$2y$10$wKQ5Xe5yHT2OmqqEqYHui.oINYAnMaoO2BCSkdBIEjpCdbFhlSkLS', 'dummy@email.com', 'e271390a9e497368fb3e1aea775b8292', 1, '0', '2016-05-02 21:00:00'),
 (5, 'Dummy', 'last 2', 'dummyDoc2', '$2y$10$yMmsT8j4ITUOrKt48sECEuOj80gVaJNhe.sZZ6D5.fQGXpTM.NWrm', 'dummy2@email.com', '33d78dd73a1ae69e3eb6bb77212e132a', 1, '0', '2016-05-02 21:00:00'),
-(6, 'Dummy', 'last 3', 'dummyDoc3', '$2y$10$/p37CiI1Mk4z.0pjTZbrCufbBUdlf.83aKBqcWPcj2d/w9f.cGZle', 'dummy3@email.com', '48eec844f119889db406d85c24607f8b', 1, '0', '0000-00-00 00:00:00'),
-(7, 'Dummy', 'last 4', 'dummyDoc4', '$2y$10$xsXeB/msKajS6RHW6Vbr0.5PJOwtrpBTfD2ncRCTBEG2og1u8tM5C', 'dummy4@email.com', '54d6b71ef8a9986b3825eac28c3de8b1', 1, '0', '0000-00-00 00:00:00'),
+(6, 'Dummy', 'last 3', 'dummyDoc3', '$2y$10$/p37CiI1Mk4z.0pjTZbrCufbBUdlf.83aKBqcWPcj2d/w9f.cGZle', 'dummy3@email.com', '48eec844f119889db406d85c24607f8b', 1, '0', '2016-05-02 21:00:00'),
+(7, 'Dummy', 'last 4', 'dummyDoc4', '$2y$10$xsXeB/msKajS6RHW6Vbr0.5PJOwtrpBTfD2ncRCTBEG2og1u8tM5C', 'dummy4@email.com', '54d6b71ef8a9986b3825eac28c3de8b1', 1, '0', '2016-05-02 21:00:00'),
 (8, 'Dummy', 'last 5', 'dummyDoc5', '$2y$10$0L.ibreV7UrtPNHYzUZZtug9jZD4CI0dyn52SzSVCgQKEU2Us/QvK', 'dummy5@gmail.com', 'dc9bf4dd204eefe0d98b3e36014d4f77', 1, '0', '2016-05-25 18:31:49'),
 (9, 'Dummy', 'last6', 'dummyDoc6', '$2y$10$nJvidov9sfJBVe0sALyZteCqbeYc6Tvy2EpGr5McVB.1c0VlnLaPa', 'dummy6@gmail.com', 'b0d5cd29b95c497888dedc13dcda0e2a', 1, '0', '2016-05-25 18:32:26'),
 (10, 'Dummy', 'last7', 'dummyDoc7', '$2y$10$YFoEUNeMcQDFlEDpmpvbrOnrgK2m4oJNcln5sbGGMcYezZ3.Z8XUm', 'dummy7@gmail.com', 'b4f46ba62e3ec9187f124dff20babf2b', 1, '0', '2016-05-25 18:32:44'),
@@ -477,7 +517,14 @@ INSERT INTO `Users` (`userID`, `firstName`, `lastName`, `username`, `password`, 
 (12, 'Dummy', 'last9', 'dummyDoc9', '$2y$10$wfEhMZmxsfMPCZC971Xtne0LALm7u6K4aEdAy/7MlipilJ/6HtdQK', 'dummy9@gmail.com', '5454c6ff1fad4fb2f0fb40bbeb72da9e', 1, '0', '2016-05-25 18:36:50'),
 (13, 'Dummy', 'last10', 'dummyDoc10', '$2y$10$nTkzTBqwyDr2ltLJBZwFmeI0lnjWgRCh4onP3kiAANNgsfVOfSx92', 'dummy10@gmail.com', '7426455d89564e71f6da59ff020c0acf', 1, '0', '2016-05-25 18:37:05'),
 (14, 'Dummy', 'last11', 'dummyDoc11', '$2y$10$KhABsDElcWLLBTTFxq1iqe5JlCxJUfyupZJe07HuuiU2h17G/OwMS', 'dummy11@gmail.com', 'ecfcf58791bf5a622f42c48bc2da74be', 1, '0', '2016-05-25 18:37:16'),
-(15, 'Dummy', 'last12', 'dummyDoc12', '$2y$10$6b6rfvY.Vm5wfSbA2niB2.3Tb1vMAOxKSqYnZYVRRhSeugyWjlKDW', 'dummy12@gmail.com', '3819ff62f36cb9039c81406c65291be0', 1, '0', '2016-05-25 18:37:30');
+(15, 'Dummy', 'last12', 'dummyDoc12', '$2y$10$6b6rfvY.Vm5wfSbA2niB2.3Tb1vMAOxKSqYnZYVRRhSeugyWjlKDW', 'dummy12@gmail.com', '3819ff62f36cb9039c81406c65291be0', 1, '0', '2016-05-25 18:37:30'),
+(16, 'Dummy', 'test 1234', 'test1234', '$2y$10$4vxiBoNV5qzF37t9BusLVecaEEh5XrXPaHr2dNkldDqu6dDh.s3eu', 'asdfoijasdf@abv.bg', '246c616ecae24dd7db2c585fa50c01b8', 1, '0', '2016-09-07 01:39:06'),
+(17, 'qwer', 'asdf', 'asdtasdtas', '$2y$10$M6LMpho.4BG53Pypzwzsle9GU77sefmv6Od.DzL0//3jS02BHsYJy', 'asdfoijasdf1234@abv.bg', 'f5609862078f2bf60c19e602a15652b7', 1, '0', '2016-09-08 05:20:28'),
+(18, 'qwrqwrqw', 'asdfasdf', 'new test 1234', '$2y$10$sPsp.B0hxnTxUGhFxDu/NuhIC2CBj0ZZlwZOAFSe/ztaN1fUJSoMW', 'alabala1@a.bv.bg', '1e229807fb60f2e6e17d2e270471b4fa', 1, '0', '2016-09-08 05:24:21'),
+(19, 'alabala', 'alsdkjasdf', 'TEst 159595', '$2y$10$0kmLoX0sw4yiOScoCAXPoOicb56gAR7OngjLFpyrioNmU56rjnusG', 'aasdfoij@abv.bg', 'e2742bade69a2f655f78bcf27ec44d67', 1, '0', '2016-09-09 22:45:27'),
+(20, 'qwoeij', 'oijfrfrffr', 'new test 15996', '$2y$10$DJtSd/XVPM2iE5xFzF61t.SN4XZOD0pJ/SntXCDhFipdhs8U8olIi', 'asdofij@abv.bg', '6ac24b8a68698616aeedd1310b9d6f3f', 1, '0', '2016-09-09 22:50:14'),
+(21, 'fwqoifwj', 'oijfroierf', 'fqwefqwef', '$2y$10$yKa0hDw1SCrhMHc530yl0OD4Iiy0hHqZyAoOWUCpiPgosw2HRDwTe', 'fqfqwefqwef@abv.bg', 'd2e52fde2b269c613e2abf5fa99ed09f', 1, '0', '2016-09-09 22:51:51'),
+(22, 'wtoejiwteoji', 'joierwojieo', '7456745', '$2y$10$xxqCer45BuVc1a/qV3bmbeaqILSqXM3dK3VzTHWAYk8r5ma5rc5Jm', 'gwergwe@abv.bg', '4b3b153b4c4131c544ab2f445c2ce56f', 1, '0', '2016-09-09 22:55:22');
 
 --
 -- Constraints for dumped tables
